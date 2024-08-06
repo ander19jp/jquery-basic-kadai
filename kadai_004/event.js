d6a7cd11b2c94a5b3cdff14dfb39a6a827f35939
ander19jp <ander19jp@gmail.com>
2024-08-06 15:51:16 +0900

    課題提出

---
+++ kadai_004/event.js
@@ -0,0 +1,14 @@
+
+
+    $(window).on('load', () => {
+        console.log('loadイベントが発生しました');
+        
+    });
+
+    $(window).on('scroll', () => {
+        console.log('scrollイベントが発生しました');
+        
+    });
+
+
+  
